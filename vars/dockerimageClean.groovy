b673d4d2b72694c38fb6990aec647885560b0f75
Phaneendra-manthena <phani.manthena27@gmail.com>
2023-04-05 18:31:34 +0530

    groovy files added

--- vars/dockerimageClean.groovy
+++ vars/dockerimageClean.groovy
@@ -1,4 +1,4 @@
-def call(String project, String hubUser) {
+def call(String project, String ImageTag, String hubUser) {
     sh "docker rmi ${hubUser}/${project}:${ImageTag}"
      sh "docker rmi ${hubUser}/${project}:latest"
  }
