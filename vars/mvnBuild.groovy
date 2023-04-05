b6ac53b2e31ba395d2643f8d9a164d1cc0fbe540
Phaneendra-manthena <phani.manthena27@gmail.com>
2023-04-05 16:50:24 +0530

    groovy files added

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,3 +1,9 @@
 def call(){
     sh 'mvn clean install'
 }
+post {
+  success {
+     echo 'Now Archiving...'
+        archiveArtifacts artifacts: '**/target/*.war'
+                }
+            }
